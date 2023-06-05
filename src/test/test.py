917ee9abdec13d41f080721654f4e77a81f033e3
Alireza Kavianifar <alireza2011110@gmail.com>
2023-06-06 00:04:55 +0330

    r

--- src/test/test.py
+++ src/test/test.py
@@ -6,6 +6,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 
 
+PATH_DIR = r'D:\projects\text-classification\text-classification\repo\sabtenam.xlsx'
+
+
 def calculate_accuracy():
     paths = r'D:\projects\text-classification\text-classification\repo\test_sabtenam4.xlsx'
     df = pd.read_excel(paths)
@@ -22,74 +25,67 @@ def calculate_accuracy():
 def preprocess(text):
     return text[:5]
 
-PATH_DIR = r'D:\projects\text-classification\text-classification\repo\sabtenam.xlsx'
-dict_labels = {}
 
-df_sabtenam = pd.read_excel(PATH_DIR, sheet_name='Sheet1')
-df_sabtenam['gasht'] = df_sabtenam['کد پستی'].map(preprocess)
-df_gasht = pd.read_excel(PATH_DIR, sheet_name='Sheet2')
-df_gasht = df_gasht.astype('str')
-df_sabtenam.dropna(inplace=True)
-df_sabtenam = df_sabtenam.astype('str')
-df_merged = df_sabtenam.merge(
-    df_gasht, how='inner', left_on='gasht', right_on='گشت پستی')
+def prepare_data(path=PATH_DIR):
 
+    dict_labels = {}
 
-labels = df_merged['کد اداره امور مالیاتی'].unique()
+    df_sabtenam = pd.read_excel(PATH_DIR, sheet_name='Sheet1')
+    df_sabtenam['gasht'] = df_sabtenam['کد پستی'].map(preprocess)
+    df_gasht = pd.read_excel(PATH_DIR, sheet_name='Sheet2')
+    df_gasht = df_gasht.astype('str')
+    df_sabtenam.dropna(inplace=True)
+    df_sabtenam = df_sabtenam.astype('str')
+    df_merged = df_sabtenam.merge(
+        df_gasht, how='inner', left_on='gasht', right_on='گشت پستی')
 
+    labels = df_merged['کد اداره امور مالیاتی'].unique()
 
-for index, item in enumerate(labels):
-    dict_labels[item] = index
+    for index, item in enumerate(labels):
+        dict_labels[item] = index
 
-df_merged['edare_num'] = df_merged['کد اداره امور مالیاتی'].map(dict_labels)
+    df_merged['edare_num'] = df_merged['کد اداره امور مالیاتی'].map(
+        dict_labels)
 
-
-X_train, X_test, y_train, y_test = train_test_split(df_merged['آدرس'],
-                                                    df_merged['edare_num'],
-                                                    test_size=0.2,
-                                                    random_state=7)
-
-clf = Pipeline([
-    ('vectorizer_bow', CountVectorizer()),
-    ('Multi NB', MultinomialNB())
-])
-
-clf.fit(X_train, y_train)
-
-y_pred = clf.predict(X_test)
-
-print(classification_report(y_test, y_pred))
-
-pred_labels = []
-real_labels = []
-for item in y_test:
-    for key, value in dict_labels.items():
-        if str(item) == str(value):
-            pred_labels.append(key)
-
-for item in y_pred:
-    for key, value in dict_labels.items():
-        if str(item) == str(value):
-            real_labels.append(key)
-
-X_test[:20].tolist()
-y_test[:20].tolist()
-y_pred[:20].tolist()
-
-new_df = pd.DataFrame({'X_test': X_test, 'y_test': y_test, 'y_pred': y_pred,
-                       'pred_labels': pred_labels, 'real_labels': real_labels})
-
-new_df.to_excel(
-    r'D:\projects\text-classification\text-classification\repo\test_sabtenam4.xlsx')
-
-lst_labels = []
-for key, value in dict_labels.items():
-    lst_labels.append([key, value])
-
-dict_labels_df = pd.DataFrame(lst_labels, columns=['key', 'value'])
+    X_train, X_test, y_train, y_test = train_test_split(df_merged['آدرس'],
+                                                        df_merged['edare_num'],
+                                                        test_size=0.2,
+                                                        random_state=7)
+    return X_train, X_test, y_train, y_test, dict_labels
 
 
-y_test_ = y_test.merge()
+def create_final_report(dict_labels, y_pred, y_test):
+    pred_labels = []
+    real_labels = []
+    for item in y_test:
+        for key, value in dict_labels.items():
+            if str(item) == str(value):
+                pred_labels.append(key)
 
-df_merged = new_df.merge(dict_labels_df, how='left',
-                         left_on='y_test', right_on='value')
+    for item in y_pred:
+        for key, value in dict_labels.items():
+            if str(item) == str(value):
+                real_labels.append(key)
+
+    new_df = pd.DataFrame({'X_test': X_test, 'y_test': y_test, 'y_pred': y_pred,
+                           'pred_labels': pred_labels, 'real_labels': real_labels})
+
+    new_df.to_excel(
+        r'D:\projects\text-classification\text-classification\repo\test_sabtenam4.xlsx')
+
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test, dict_labels = prepare_data(path=PATH_DIR)
+
+    clf = Pipeline([
+        ('vectorizer_bow', CountVectorizer()),
+        ('Multi NB', MultinomialNB())
+    ])
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print(classification_report(y_test, y_pred))
+
+    create_final_report(dict_labels, y_pred, y_test)
